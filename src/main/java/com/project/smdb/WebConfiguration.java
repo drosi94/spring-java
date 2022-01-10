abab2f19f5980fff9b1faef5875c1d78fe1be329
vasileios.drosatos <vasileios.drosatos@workable.com>
2022-01-10 21:38:35 +0200

    Add log for requests

--- src/main/java/com/project/smdb/WebConfiguration.java
+++ src/main/java/com/project/smdb/WebConfiguration.java
@@ -2,8 +2,10 @@ package com.project.smdb;
 
 import com.project.smdb.converter.MovieTypeConverter;
 import com.project.smdb.converter.PersonRoleConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +14,17 @@ public class WebConfiguration implements WebMvcConfigurer  {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new MovieTypeConverter());
         registry.addConverter(new PersonRoleConverter());
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
     }
 }
